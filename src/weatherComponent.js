88ea71cf2b0351fca79a3801027aaab3de298f17
Jesna <jesnak>
2021-12-22 02:08:40 +0530

    FInal commit

--- src/weatherComponent.js
+++ src/weatherComponent.js
@@ -7,8 +7,9 @@ export default function Weather() {
 
   const [date, setDate] = useState(null);
   const [time, setTime] = useState(null);
-  const [loading, setLoadingState] = useState(false);
+  const [loading, setLoadingState] = useState(true);
 
+  const [country, setCountry] = useState('Default Country');
   const [city, setCity] = useState('Default City');
   const [temperature, setTemperature] = useState(0);
   const [maxTemp, setMaxTemp] = useState(0);
@@ -34,12 +35,17 @@ export default function Weather() {
       setDate(date);
       setTime(time);
 
-      setLoadingState(true)
-      const res = await fetch('https://fcc-weather-api.glitch.me/api/current?lat=35&lon=139');
+      const location = {
+        lat: '9.931233',
+        lon: '76.267303'
+      }
+
+      const res = await fetch(`https://fcc-weather-api.glitch.me/api/current?lat=${location.lat}&lon=${location.lon}`);
       const data = await res.json();
-      console.log(data);
+      if(res)
       setLoadingState(false)
 
+      setCountry(data.sys.country)
       setCity(data.name);
       setTemperature(data.main.temp);
       setFeeslLike(data.main.feels_like);
@@ -61,45 +67,47 @@ export default function Weather() {
       return loadingText
     } else {
       return (
-        <div style={{width: '600px', height: 'max-content', margin: '0 auto', padding: '16px', borderRadius: '12px', backgroundImage: 'linear-gradient(to top, #0ba360 0%, #3cba92 100%)', color: 'white', boxShadow: 'rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px' }}>
-        <p style={{textTransform: 'uppercase', marginBottom: '0'}}><strong>Current Weather @ {city}</strong></p>
-        <small style={{textTransform: 'uppercase'}}>{date} {time}</small>
+        <div>
+          <div style={{width: '600px', height: 'max-content', margin: '0 auto', padding: '16px', borderRadius: '12px', backgroundImage: 'linear-gradient(to top, #0ba360 0%, #3cba92 100%)', color: 'white', boxShadow: 'rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px' }}>
+            <p style={{textTransform: 'uppercase', marginBottom: '0'}}><strong>Current Weather @ {city}, {country}</strong></p>
+            <small style={{textTransform: 'uppercase'}}>{date} {time}</small>
 
-        <div style={{display: 'flex', flexDirection: 'row', margin: '12px 0'}}>
-        <div style={{display: 'flex', width: 'max-content', alignItems: 'center', justifyContent: 'start'}}>
-            <div style={{width: '60px', height: '60px', borderRadius: '50%', backgroundImage: temperature > 7 ? 'linear-gradient(to right, #f9d423 0%, #ff4e50 100%)':'linear-gradient(to top, #accbee 0%, #e7f0fd 100%)', marginRight: '10px', backgroundColor: '#FFE400'}} />
-            <p style={{fontSize: '36px', margin: 0}}>{temperature}<sup>℃</sup></p>
-          </div>
-          <div style={{marginLeft: '20px'}}>
-              <p style={{margin: 0, textTransform: 'uppercase'}}><b>{shortWeather}</b></p>
-              <p style={{margin: 0}}>Feels Like &nbsp;&nbsp;{feelsLike}<sup>℃</sup></p>
-          </div>
-          </div>
+            <div style={{display: 'flex', flexDirection: 'row', margin: '12px 0'}}>
+            <div style={{display: 'flex', width: 'max-content', alignItems: 'center', justifyContent: 'start'}}>
+              <div style={{width: '60px', height: '60px', borderRadius: '50%', backgroundImage: temperature > 7 ? 'linear-gradient(to right, #f9d423 0%, #ff4e50 100%)':'linear-gradient(to top, #accbee 0%, #e7f0fd 100%)', marginRight: '10px', backgroundColor: '#FFE400'}} />
+              <p style={{fontSize: '36px', margin: 0}}>{temperature}<sup>℃</sup></p>
+            </div>
+            <div style={{marginLeft: '20px'}}>
+                <p style={{margin: 0, textTransform: 'uppercase'}}><b>{shortWeather}</b></p>
+                <p style={{margin: 0}}>Feels Like &nbsp;&nbsp;{feelsLike}<sup>℃</sup></p>
+            </div>
+            </div>
 
-          <p style={{textTransform: 'unset'}}>Expect {expectedWeather}. The high will be {maxTemp}<sup>℃</sup>.</p>
-          <div style={{display: 'flex', flexDirection: 'row', marginTop: '46px', textTransform: 'uppercase'}}>
-          
-          <div style={{width: '33.33%'}}>
-              <p style={{margin: 0}}>Wind</p>
-              <small style={{fontSize: '12px'}}>{wind} km/h</small>
-          </div>
-          <div style={{width: '33.33%'}}>
-              <p style={{margin: 0}}>Humidity</p>
-              <small style={{fontSize: '12px'}}>{humidity}%</small>
-          </div>
-          <div style={{width: '33.33%'}}>
-              <p style={{margin: 0}}>Visibility</p>
-              <small style={{fontSize: '12px'}}>{visibility} KM</small>
-          </div>
-          <div style={{width: '33.33%'}}>
-          <p style={{margin: 0}}>Pressure</p>
-          <small style={{fontSize: '12px'}}>{pressure} mb</small>
-          </div>
-          <div style={{width: '33.33%'}}>
-          <p style={{margin: 0}}>Min Temp</p>
-              <small style={{fontSize: '12px'}}>{minTemp}<sup>℃</sup></small>
-          </div>
+            <p style={{textTransform: 'unset'}}>Expect {expectedWeather}. The high will be {maxTemp}<sup>℃</sup>.</p>
+            <div style={{display: 'flex', flexDirection: 'row', marginTop: '46px', textTransform: 'uppercase'}}>
+            
+            <div style={{width: '33.33%'}}>
+                <p style={{margin: 0}}>Wind</p>
+                <small style={{fontSize: '12px'}}>{wind} km/h</small>
+            </div>
+            <div style={{width: '33.33%'}}>
+                <p style={{margin: 0}}>Humidity</p>
+                <small style={{fontSize: '12px'}}>{humidity}%</small>
+            </div>
+            <div style={{width: '33.33%'}}>
+                <p style={{margin: 0}}>Visibility</p>
+                <small style={{fontSize: '12px'}}>{visibility} KM</small>
+            </div>
+            <div style={{width: '33.33%'}}>
+            <p style={{margin: 0}}>Pressure</p>
+            <small style={{fontSize: '12px'}}>{pressure} mb</small>
+            </div>
+            <div style={{width: '33.33%'}}>
+            <p style={{margin: 0}}>Min Temp</p>
+                <small style={{fontSize: '12px'}}>{minTemp}<sup>℃</sup></small>
+            </div>
 
+            </div>
           </div>
         </div>
       )
