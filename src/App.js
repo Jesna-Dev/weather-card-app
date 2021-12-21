444c8e437124bb311e54a26e4038e3ca039fc16b
Jesna <jesnak>
2021-12-22 01:39:40 +0530

    clean

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
 import React from 'react';
-import { useEffect, useState } from 'react';
 import './App.css';
 import Weather from './weatherComponent';
 
 function App() {
-  const [date, setDate] = useState(null);
-  useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
-    }
-    getDate();
-  }, []);
   return (
     <main>
       <h1>WEATHER FORECAST</h1>
@@ -20,5 +10,4 @@ function App() {
     </main>
   );
 }
-
 export default App;
